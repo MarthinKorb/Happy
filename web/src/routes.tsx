b298ef41fb62357d6767fbc75ec1d3f1c0c4f7ee
Marthin Korb <marthinkorb@gmail.com>
2020-10-15 23:52:28 -0300

    Building routes for new Components

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -4,13 +4,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing} />
-                <Route path="/orphanages" component={OrphanagesMap} />
+                <Route path="/orphanages" exact component={OrphanagesMap} />
+                
+                <Route path="/orphanages/create" exact component={CreateOrphanage} />
+                <Route path="/orphanages/:id" exact component={Orphanage} />
             </Switch>
         </BrowserRouter>
     );
